57c62019e61d0d30c6c41a092b3b9c95cc9dd4af
lmr97 <lmreid1997@gmail.com>
2025-01-27 22:35:13 -0700

    rejection handling

--- custom-backend/src/main.rs
+++ custom-backend/src/main.rs
@@ -1,5 +1,12 @@
-use std::{env, error::Error as StdError};
-use warp::Filter;
+use std::{
+    env, 
+    error::Error as StdError,
+    convert::Infallible
+};
+use warp::http::StatusCode;
+use warp::{reject, Filter, Rejection, Reply};
+
+static LOCAL_ROOT: &str = "/home/martin/archie-server";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn StdError>> {
@@ -8,14 +15,14 @@ async fn main() -> Result<(), Box<dyn StdError>> {
     
     let home = warp::get()
         .and(warp::path::end())
-        .and(warp::fs::file("../home.html"));
+        .and(warp::fs::file(format!("{}/home.html", LOCAL_ROOT)));
 
     let image = warp::get()
         .and(warp::path("images"))
-        .and(warp::fs::dir("../images/"))
+        .and(warp::fs::dir(format!("{}/images/", LOCAL_ROOT)))
         .with(warp::compression::gzip());
 
-    let routes = home.or(image);
+    let routes = home.or(image).recover(handle_rejection);
     warp::serve(routes)
         .tls()
         .cert_path(cert)
@@ -41,4 +48,17 @@ fn get_auth_paths() -> (String, String) {
         .unwrap();
 
     (cert_file, private_key_file)
+}
+
+// very simple right now, to match the server
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
+    
+    let err_404 = warp::reply::html(format!("{}/errors/404.html", LOCAL_ROOT));
+    let err_500 = warp::reply::html(format!("{}/errors/500.html", LOCAL_ROOT));
+    
+    if err.is_not_found() {
+        Ok(warp::reply::with_status(err_404, StatusCode::NOT_FOUND))
+    } else {
+        Ok(warp::reply::with_status(err_500, StatusCode::INTERNAL_SERVER_ERROR))
+    }
 }
