b72aeebf9fe9dd482b5424323f76ba3a8362f331
lmr97 <lmreid1997@gmail.com>
2025-02-27 21:32:51 -0700

    doing proper POST then GET for logging hits

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,11 +1,39 @@
 let hitsElement = document.getElementById("hit-count");
-
 hitsElement.innerText = "Visit count loading...";
-fetch("https://archie.zapto.org/hits")
-    .then(function(response) { return response.text(); })
-    .then(function(respBody) {
-        hitsElement.innerText = ("Visits so far: " + respBody);
+
+const hit = {
+    time_stamp: new Date().toISOString().slice(0, -1),  // shave off Z
+    user_agent: navigator.userAgent
+};
+
+const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(hit)
+};
+
+// POST hit from initial page load...
+fetch(window.location.href + "hits", options)
+    .then(resp => {
+        if (!resp.ok) throw new Error(
+            `POST hit to /hits failed with status code ${resp.status}`
+        );
     })
+    .catch(error => console.log(error));
+
+// ...then GET total hits
+fetch(window.location.href + "hits")
+    .then((resp) => { 
+        if (!resp.ok) throw new Error(
+             `GET /hits failed with status code ${resp.status}`
+        );
+        return resp.text();
+    })
+    .then(result => 
+        hitsElement.innerText = `Visits so far: ${result}`
+    )
     .catch(error => {
         console.log(error);
         hitsElement.innerText = "(unable to get visit count)";
