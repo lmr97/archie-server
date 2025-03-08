f88bd8e2801a3155b94bea8f8f4056d46566f2f3
lmr97 <lmreid1997@gmail.com>
2025-03-07 23:34:14 -0700

    extended fields

--- create_db.sql
+++ create_db.sql
@@ -5,8 +5,8 @@ CREATE TABLE guestbook
 (
     id              INT NOT NULL AUTO_INCREMENT,
     dateSubmitted   DATETIME,
-    guestName       VARCHAR(30)  NOT NULL,
-    guestNote       VARCHAR(300),
+    guestName       VARCHAR(50)  NOT NULL,
+    guestNote       VARCHAR(1000),
     PRIMARY KEY     (id) 
 );
 
