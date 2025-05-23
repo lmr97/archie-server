6d1b245a326a740d2fa33277529a067235df7194
lmr97 <lmreid1997@gmail.com>
2025-05-22 22:17:05 -0600

    runs with main

--- lb-app/healthcheck.py
+++ lb-app/healthcheck.py
@@ -1,21 +1,48 @@
 """
-A simple health check for Docker.
+A simple health check for the Python app's Docker container.
 """
-
-import socket
 import sys
+import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def is_healthy() -> bool:
+    """
+    Performs the healthcheck.
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    sock.connect(("0.0.0.0", 3575))
+    try:
+        sock.connect(("0.0.0.0", 3575))
 
-    ping_msg = bytes('{"msg": "are you healthy?"}', 'utf-8')
-    sock.sendall(ping_msg)
+        ping_msg  = bytes('{"msg": "are you healthy?"}', 'utf-8')
+        sock.sendall(ping_msg)
+        sock.recv(2)            # recieve and discard message length bytes (it's gonna be 19)
+        response  = sock.recv(19).decode("utf-8")
+        sock.recv(7)            # get done signal with length bytes (it's gonna be 2+5 == 7)
 
-    sock.close()
+        if response != "yep, still healthy!":
+            print("[ \033[0;31mHEALTHCHECK FAILED\033[0m ]: Python app running, but sent "
+                f"unexpected response to healthcheck ping: \"{response}\"",
+                file=sys.stderr)
+            sock.close()  # redundant, but won't throw error
+            return False
+
+        sock.close()
+        return True
+
+    except ConnectionRefusedError:
+        print("[ \033[0;31mHEALTHCHECK FAILED\033[0m ]: Python app no longer running!",
+            file=sys.stderr)
+        return False
+
+
+def main():
+    """
+    Just handles exiting with the right exit code.
+    """
+    if not is_healthy():
+        sys.exit(1)
+
     sys.exit(0)
 
-except ConnectionRefusedError:
-    print("[ \033[0;31mHEALTHCHECK FAILED\033[0m ]: Python listener no longer running!")
-    sys.exit(1)
+if __name__ == "__main__":
+    main()
