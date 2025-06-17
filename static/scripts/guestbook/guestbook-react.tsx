5127100236f2356f08997bdd065619b382201ea9
lmr97 <lmreid1997@gmail.com>
2025-06-16 23:19:12 -0600

    factoring out functions from component

--- static/scripts/guestbook/guestbook-react.tsx
+++ static/scripts/guestbook/guestbook-react.tsx
@@ -1,6 +1,59 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { type Guestbook, type GuestbookEntry, type EntryReceipt } from '../server-types.ts';
+
+
+/* 
+    Since these functions and constants don't depend on component 
+    states, they can be defined outside of it. It also saves time
+    and computing power, since the functions will not be redefined
+    on every render.
+*/
+
+const MAX_NAME_LENGTH = 100;  // byte values
+const MAX_NOTE_LENGTH = 1000;
+const textEncoder = new TextEncoder();
+
+function getDate(entry: GuestbookEntry): Date {
+    
+    if (!entry.timeStamp) {
+        return new Date();
+    } else {
+        // Z, denoting UTC, gets stripped at DB. Needs to be added
+        // back in for correct parsing in JS.
+        return new Date(entry.timeStamp+"Z");
+    }
+}
+
+function logAlertErrorSending(errorMessage: string) {
+    console.error(errorMessage);
+    alert("There was an error in sending guestbook entry, sorry. \
+        \nI'll look into this issue as soon as I can!");
+}
+
+function displayErrorMsg(newEntry: GuestbookEntry) {
+    // measures byte-length of data, courtesy of this SO post:
+    // https://stackoverflow.com/questions/5515869/string-length-in-bytes-in-javascript
+    
+    if (textEncoder.encode(newEntry.name).length > MAX_NAME_LENGTH) {
+        // this will probably not run since the HTML limits the input to MAX_NAME_LENGTH,
+        // but it could, since MAX_NAME_LENGTH measures bytes, and the HTML only limits 
+        // the number of "characters".
+        alert("The name you entered it too long! Is there a shorter way you can identify yourself?\n\n\
+            Note: The limit is 100 bytes, which can fit 100 non-accented Latin characters. \
+            This limit will be reached sooner, however, with accented or non-Latin characters, \
+            so you might get less than 100 characters in that case.".replaceAll("  ", ""));
+        return;
+    }
+    if (textEncoder.encode(newEntry.note).length > MAX_NOTE_LENGTH) {
+        alert("The note you entered it too long! Is there a shorter note you can leave? \
+            Multiple notes are also welcome!\n\n\
+            Note: The limit is 1000 bytes, which can fit 1000 non-accented Latin characters. \
+            This limit will be reached sooner, however, with accented or non-Latin characters, \
+            so you might get less than 1000 characters in that case.".replaceAll("  ", ""));
+        return;
+    }
+}
 
 
 export default function GuestbookApp() {
@@ -36,8 +89,10 @@ export default function GuestbookApp() {
     // this will allow instant updates to guestbook as it appears on the page
     const [guestbook, setGuestbook]: [Guestbook, Function] = useState(emptyGuestbook);
 
-    // this ID is the one given by the DB
-    const [latestEntryId, setLatestEntryId]: [string, Function] = useState('');
+    // this ID is the one given by the DB. It does not need to trigger a re-render,
+    // so it can be a Ref, not a part of the state (the `guestbook` variable above 
+    // is what carries the component state)
+    const latestEntryId: React.RefObject<string> = useRef('');
     
     useEffect(
         () => {
@@ -59,20 +114,14 @@ export default function GuestbookApp() {
         }
         const allEntries = guestbook.guestbook.map(
             (entry: GuestbookEntry) => {
-                var entryDate: Date;
-                if (!entry.timeStamp) {
-                    entryDate = new Date();
-                } else {
-                    // Z, denoting UTC, gets stripped at DB. Needs to be added
-                    // back in for correct parsing in JS.
-                    entryDate = new Date(entry.timeStamp+"Z");
-                }
+                const entryDate: Date = getDate(entry);
                 const entryTimeString = entryDate.toLocaleString("en-US", timeOptions);
                 return (
                     // add "your-entry" class only if the ID matches the latest posted entry
                     <div key={entry.id} 
-                        className={"shine-box" + (entry.id == latestEntryId ? " your-entry": "")}
-                        data-testid="shine-box">
+                        className={"shine-box" + (entry.id == latestEntryId.current ? " your-entry": "")}
+                        data-testid="shine-box"
+                        >
                         <div className="guestbook-entry"
                             >
                             <p className="entry entry-time" data-testid={"entry-time"+entry.id}>
@@ -94,17 +143,8 @@ export default function GuestbookApp() {
 
     /* Entry Box */
     function GuestEntryForm() {
-
-        const MAX_NAME_LENGTH = 100;  // byte values
-        const MAX_NOTE_LENGTH = 1000;
-        const textEncoder = new TextEncoder();
+        
         const [charCount, setCharCount]: [number, Function] = useState(0);
-
-        function logAlertErrorSending(errorMessage: string) {
-            console.error(errorMessage);
-            alert("There was an error in sending guestbook entry, sorry. \
-                \nI'll look into this issue as soon as I can!");
-        }
 
         // updates entries, locally and remotely
         function updateEntries(submitEvent: React.FormEvent<HTMLFormElement>) {
@@ -125,27 +165,7 @@ export default function GuestbookApp() {
                 note: formData.get("note") as string,
             }
 
-            // measures byte-length of data, courtesy of this SO post:
-            // https://stackoverflow.com/questions/5515869/string-length-in-bytes-in-javascript
-            
-            if (textEncoder.encode(newEntry.name).length > MAX_NAME_LENGTH) {
-                // this will probably not run since the HTML limits the input to MAX_NAME_LENGTH,
-                // but it could, since MAX_NAME_LENGTH measures bytes, and the HTML only limits 
-                // the number of "characters".
-                alert("The name you entered it too long! Is there a shorter way you can identify yourself?\n\n\
-                    Note: The limit is 100 bytes, which can fit 100 non-accented Latin characters. \
-                    This limit will be reached sooner, however, with accented or non-Latin characters, \
-                    so you might get less than 100 characters in that case.".replaceAll("  ", ""));
-                return;
-            }
-            if (textEncoder.encode(newEntry.note).length > MAX_NOTE_LENGTH) {
-                alert("The note you entered it too long! Is there a shorter note you can leave? \
-                    Multiple notes are also welcome!\n\n\
-                    Note: The limit is 1000 bytes, which can fit 1000 non-accented Latin characters. \
-                    This limit will be reached sooner, however, with accented or non-Latin characters, \
-                    so you might get less than 1000 characters in that case.".replaceAll("  ", ""));
-                return;
-            }
+            displayErrorMsg(newEntry);
 
             fetch("/guestbook/entries", 
                 {
@@ -166,10 +186,7 @@ export default function GuestbookApp() {
                         return resp.json()
                     }
                 })
-                .then((newIdJson: EntryReceipt) => {
-                    const newEntryId = newIdJson.id;
-                    setLatestEntryId(newEntryId);
-                })
+                .then((newIdJson: EntryReceipt) => { latestEntryId.current = newIdJson.id; })
                 .catch(err => { logAlertErrorSending(`POST to server failed. ${err}`) });
             
             // THE BIG FIX: this Guestbook object needs to be set by passing 
