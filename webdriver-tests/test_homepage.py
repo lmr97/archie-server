e163cb22190e8fa0722c3722e169eb67d145c0a2
lmr97 <lmreid1997@gmail.com>
2025-06-28 19:59:36 -0600

    adding first full-server integration test

---
+++ webdriver-tests/test_homepage.py
@@ -0,0 +1,91 @@
+import os
+import subprocess
+import platform
+import numpy as np
+from selenium import webdriver as wd
+from selenium.webdriver import ActionChains
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+drivers = [wd.Firefox()]   # the default, becasue I like Linux
+
+match platform.system():
+    case "Windows":
+        drivers[0] = wd.Edge()
+    case "Darwin":          # MacOS
+        drivers[0] = wd.Safari()
+    case _:
+        # see if Chrome is already there
+        try:
+            drivers.append(wd.Chrome())
+        except Exception:
+
+            # Otherwise, install Chrome on Linux to test there as well
+            print("Downloading Chrome installer...")
+            subprocess.run(
+                ["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"]
+            )
+            print("Done.")
+
+            print("Installing Chrome...")
+            subprocess.run(["sudo", "dpkg", "-i" "google-chrome-stable_current_amd64.deb"])
+            print("Done.")
+
+            drivers.append(wd.Chrome())
+
+
+root_url = os.getenv("DOCKER_SERVER_URL")   # has no trailing slash
+
+[d.get(root_url) for d in drivers]
+
+
+def test_hit_count():
+
+    correct_count = 7  # native browser will run first, then Chrome if present
+    
+    for drv in drivers:
+        hit_count = drv.find_element(By.ID, "hit-count")
+
+        assert str(correct_count) in hit_count.text, f"failed for {drv.name}"
+
+        correct_count += 1
+
+
+# test whether the logo enlarges when moused over,
+# and shows the message from out behind it
+def test_react_logo():
+    for drv in drivers:
+        react_logo_div: WebElement = drv.find_element(By.CSS_SELECTOR, "div.interact-synchronizer")
+        react_logo: WebElement     = react_logo_div.find_element(By.CSS_SELECTOR, "svg.react-logo")
+        viewbox_before: str        = react_logo.get_dom_attribute("viewBox")
+        viewbox_dims_before        = np.array(viewbox_before.split(" "), dtype=float)  # using Numpy for comparisons
+
+        # hover over
+        ActionChains(drv) \
+            .move_to_element(react_logo_div) \
+            .perform()
+
+        # give it a moment to change in up to 2 browsers (it's gradual)
+        logo_msg = react_logo_div.find_element(By.CSS_SELECTOR, "p.react-msg")
+        wait     = WebDriverWait(drv, 5.0)
+        wait.until(ec.visibility_of(logo_msg))
+
+        viewbox_after: str = react_logo.get_dom_attribute("viewBox")
+        viewbox_dims_after = np.array(viewbox_after.split(" "), dtype=float)
+
+        # did the logo enlarge? 
+        assert (viewbox_dims_before[:2] < viewbox_dims_after[:2]).all(), f"failed for {drv.name}"
+        assert (viewbox_dims_before[2:] > viewbox_dims_after[2:]).all(), f"failed for {drv.name}"
+
+        # did the message appear?
+        assert react_logo_div \
+            .find_element(By.CSS_SELECTOR, "p.react-msg") \
+            .is_displayed(), \
+            f"failed for {drv.name}"
+    
+def test_teardown():
+    for drv in drivers:
+        drv.quit()
